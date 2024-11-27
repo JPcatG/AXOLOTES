086fe98256774e0003cc618291e33d8f49a0cc1c
TyFox756 <tylerthefox756@outlook.com>
2024-11-27 13:28:57 -0600

    Proyecto UNITY
    n

---
+++ Assets/First person controller/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+
+
+    void Update()
+    {
+        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+    }
+}
